d43414e068474f5d3b717ff248eba3ae2a48000c
LordOlumide <olumideolubosede@yahoo.com>
2022-09-09 20:37:28 +0100

    still on it

--- lib/user_interface/screens/home_screen/home_screen.dart
+++ lib/user_interface/screens/home_screen/home_screen.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
+import 'widgets/year_card_display.dart';
 
 class HomeScreen extends StatelessWidget {
   static const screenId = 'Home screen';
-  const HomeScreen({Key? key}) : super(key: key);
+
+  HomeScreen({Key? key}) : super(key: key);
+
+  List<String> strings = ['hi', 'lo', 'kids'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-
-          ],
-        ),
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(15, 40, 0, 10),
+            child: const Text(
+              'CGPA Calculator',
+              style: TextStyle(),
+            ),
+          ),
+          ...strings.map((e) => ListTile(title: Text(e))).toList(),
+          ExpansionTile(
+            title: Text('title'),
+            children: strings.map((e) => ListTile(title: Text(e))).toList(),
+          ),
+        ],
       ),
     );
   }
--- lib/user_interface/screens/home_screen/widgets/year_card_display.dart
+++ lib/user_interface/screens/home_screen/widgets/year_card_display.dart
@@ -1,57 +1,48 @@
 import 'package:flutter/material.dart';
 
 class YearCardDisplay extends StatelessWidget {
-  String yearTitle;
-  int courseCount; // number of courses currently entered
-  int courseUnits; // number of units
-  int yearGPA;
+  String yearTitle;// number of units
+  double? yearGPA;
 
   YearCardDisplay({
     Key? key,
     required this.yearTitle,
-    required this.courseCount,
-    required this.courseUnits,
-    required this.yearGPA,
+    this.yearGPA,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          yearTitle,
-          style: TextStyle(),
-        ),
-        GestureDetector(
-          onTap: () {},
-          child: Container(
-            decoration: BoxDecoration(
-              boxShadow: const [
-                BoxShadow(
-                  offset: Offset(1, 1),
-                ),
-              ],
-              borderRadius: BorderRadius.circular(5),
-            ),
-            child: Row(
-              children: [
-                Text(
-                  'Courses: \n$courseCount',
-                  style: TextStyle(),
-                ),
-                Text(
-                  'Units: \n$courseUnits',
-                  style: TextStyle(),
-                ),
-                Text(
-                  'GPA: \n$yearGPA',
-                  style: TextStyle(),
-                ),
-              ],
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      child: Column(
+        children: [
+          Text(
+            yearTitle,
+            style: TextStyle(),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: const [
+                  BoxShadow(
+                    offset: Offset(1, 1),
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Row(
+                children: [
+                  Text(
+                    'GPA: \n$yearGPA',
+                    style: TextStyle(),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
